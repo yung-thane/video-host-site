5bd2738fd769943da5a1748eaa4719fb3fc4e0bc
Ronald Lemus <ronaldlemus@Ronalds-MBP.lan>
2022-04-27 15:15:50 -0400

    Initial commit

---
+++ Backend/src/test/java/com/freeproject/lemus/videohostsite/VideoHostSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freeproject.lemus.videohostsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoHostSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
